8eeca24095e0ef3b9acd7dc01d7101d2b1973d46
vipres <244570102@qq.com>
2021-07-06 15:36:12 +0800

    feat: init repo on 20210706

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from '@/pages/home'
+import '@/styles/index.less'
+
+ReactDOM.render(<Home />, document.getElementById('root'))
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <>
+        <h1>This is index home page.</h1>
+      </>
+    )
+  }
+}
